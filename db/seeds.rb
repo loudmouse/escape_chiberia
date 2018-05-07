b5806b8f10d8c005ae8ae2cd19c1c1daf78aea76
Nolan Neeley <nolanneeley@gmail.com>
2018-05-07 12:09:42 -0500

    create Airport model, migration, and seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+airport_code_list = [
+  [ "Chicago", "MDW" ],
+  [ "Austin", "AUS" ],
+  [ "Nashville", "BNA" ],
+  [ "Charleson", "CHS" ],
+  [ "Charlotte", "CLT" ],
+  [ "Cancun", "CUN" ],
+  [ "Los Angeles", "LAX" ],
+  [ "New Orleans", "MSY" ]
+]
+
+airport_code_list.each do |city, airport_code|
+  Airport.create( city: city, airport_code: airport_code )
+end
